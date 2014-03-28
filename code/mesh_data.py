ff48c6e5d3a680a1124586f20a954eb0c3a095f6
Kathy Lin <bluebandedgobi9353@gmail.com>
2014-03-28 17:40:10 -0400

    added CSV data for median value per square foot to json properties

--- code/mesh_data.py
+++ code/mesh_data.py
@@ -2,6 +2,7 @@ import json
 import csv
 
 id_county_dict = {}
+name_data_dict = {}
 
 def read_TSV(file):
     with open(file) as tsv:
@@ -13,6 +14,12 @@ def read_TSV(file):
             county = line[1]
             id_county_dict[idnum] = county
 
+def read_CSV(file):
+    with open(file) as csvfile:
+        for line in csv.reader(csvfile):
+            name_data_dict[line[0]] = line[5:]
+
+
 def mesh_data(filename,output):
     json_data = open(filename)
     data = json.load(json_data)
@@ -21,12 +28,20 @@ def mesh_data(filename,output):
     for county in counties:
         idnum = county["id"]
         if idnum in id_county_dict.keys():
-            county["properties"] = {"name": id_county_dict[idnum]}
+            name = id_county_dict[idnum]
+            try: 
+                medianValueSqFt = name_data_dict[name]
+            except KeyError:
+                medianValueSqFt = []
+            county["properties"] = {"name": name, "medianValueSqFt": medianValueSqFt}
+
         else:
-            county["properties"] = {"name": "NONE"}
+            county["properties"] = {"name": "", "medianValueSqFt": []}
 
     with open(output,'wb') as fp:
         json.dump(data, fp)
 
 read_TSV("../data/topojson/us-county-names.tsv")
+read_CSV("../data/zillow/county/HomesSoldAsForeclosures-Ratio.csv")
 mesh_data("../data/topojson/us-states-and-counties.json","../data/topojson/named-us-states-and-counties.json")
+
